55827bbcb78e5d07097ac65ee51b08b6a5299471
kisate <dhkas@mail.ru>
2019-11-27 00:57:46 +0300

    asd

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from lib import *
-import collections
+import collections, secrets
 runs = []
 for _ in range(10):
     print(gen_primes())
 for _ in range(10):
     print(gen_pseudoprime())
+
+for _ in range(10):
+    key = rsa_gen_keys()
+    t = secrets.randbits(100)
+    print(t == rsa_decrypt(key[0], key[4], rsa_encrypt(key[0], key[3], t)))
